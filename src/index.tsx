3809a7896969452628cdba654998d88854f8b826
FarazAfridi <farazafridi5159@gmail.com>
2020-12-27 17:16:05 +0500

    diaries app completed

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setupServer } from "./services/mirage/server";
+import { Provider } from 'react-redux';
+import store from './store';
+  setupServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
